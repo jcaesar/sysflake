4de9dfe372c0de232c3f186d3ff01523081bae21
Julius <michaelis@fujitsu.com>
2023-11-27 10:07:53 +0900

    .

--- flake.nix
+++ flake.nix
@@ -1,18 +1,18 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-
-  outputs = { self, nixpkgs }: {
-    # VM on gemini
-    nixosConfigurations.capri = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs }: let
+    fromCfg = cfg: nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./configuration.nix
+        cfg
         ({ ... }: {
           nix.registry.nixpkgs.flake = nixpkgs;
           system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
         })
       ];
     };
+  in {
+    nixosConfigurations.capri = fromCfg ./configuration.nix; # VM on gemini
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
   };
 }
