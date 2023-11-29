a5a2dab554ee86010d77ab9995df3adac23de9d2
Julius Michaelis <michaelis@jp.fujitsu.com>
2023-11-29 14:25:32 +0900

    Revert reversion

--- apply.sh
+++ apply.sh
@@ -4,12 +4,18 @@
 
 set -euo pipefail
 
+if ! which nixos-rebuild &>/dev/null; then
+	nix shell nixpkgs\#nixos-rebuild -c "$0" "$@"
+fi
+
 cd "$(dirname "$0")"
 
-for h in capri shan{2,6,7}; do
-    echo
-	echo "$h"
-	echo $(sed 's/./=/g' <<<"$h")
-	rsync -ae "ssh -q" . $h:./sysflake
-	ssh -qtt $h sudo nixos-rebuild switch --flake ./sysflake
+export NIX_SSHOPTS=-q
+
+echo -en "\ncapri\n=====\n"
+nixos-rebuild switch --target-host capri --build-host capri --use-remote-sudo --flake .\#capri
+
+for h in {2,6,7}; do
+	echo -en "\nshamo$h\n======\n"
+	nixos-rebuild switch --target-host "shamo$h" --build-host shamo2 --flake ".#shamo$h"
 done
