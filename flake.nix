aed00567441d402cb9b33dca4758adf8c11dd7d4
Julius Michaelis <michaelis@jp.fujitsu.com>
2023-12-06 10:22:08 +0900

    Switch to small channel

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11-small";
   outputs = { self, nixpkgs }:
     let
       common = import ./common.nix;
