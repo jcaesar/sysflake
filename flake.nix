a287270a1428703b005daba98923714c11b3d08d
Julius Michaelis <michaelis@fujitsu.com>
2023-12-11 21:49:40 +0900

    home manager init

--- flake.nix
+++ flake.nix
@@ -1,9 +1,8 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-
   outputs = {
     self,
     nixpkgs,
+    home-manager,
   }: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
@@ -13,9 +12,21 @@
       modules = [
         (import ./configuration.nix)
         ({...}: {nix.registry.nixpkgs.flake = nixpkgs;})
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.julius = import ./home.nix;
+        }
       ];
     };
 
     formatter.${system} = pkgs.alejandra;
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 }
---
+++ home.nix
@@ -0,0 +1,7 @@
+{
+  home.username = "julius";
+  home.homeDirectory = "/home/julius";
+  home.stateVersion = "23.11";
+
+  programs.home-manager.enable = true;
+}
