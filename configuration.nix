722057ea3b74b49a50628770f33dcdea23330fb1
Julius Michaelis <michaelis@fujitsu.com>
2023-12-11 10:54:29 +0900

    More hyprland+ime stuff

--- configuration.nix
+++ configuration.nix
@@ -79,6 +79,7 @@
     XDG_VIDEOS_DIR="$HOME/music"
   '';
   environment.variables.EDITOR = "hx";
+  environment.variables.VISUAL = "hx";
 
   programs.hyprland = {
     enable = true;
@@ -131,10 +132,42 @@
       helvum
       pulseaudio
       pavucontrol
+      dunst
+      # Hyprland stuff
+      #qt6-wayland
+      wofi
+      swww
+      hyprpaper
+      polkit-kde-agent
     ];
     shell = pkgs.nushell;
     password = "";
   };
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    QT_QPA_PLATFORM         = "wayland";
+    CLUTTER_BACKEND         = "wayland";
+    SDL_VIDEODRIVER         = "wayland";
+    MOZ_ENABLE_WAYLAND      = "1";
+    MOZ_WEBRENDER           = "1";
+    XDG_SESSION_TYPE        = "wayland";
+    XDG_CURRENT_DESKTOP     = "sway";
+    QT_QPA_PLATFORMTHEME    = "qt5ct";
+    GLFW_IM_MODULE          = "fcitx";
+    GTK_IM_MODULE           = "fcitx";
+    INPUT_METHOD            = "fcitx";
+    XMODIFIERS              = "@im=fcitx";
+    IMSETTINGS_MODULE       = "fcitx";
+    QT_IM_MODULE            = "fcitx";
+  };
+  i18n.inputMethod = {
+      enabled = "fcitx5";
+      fcitx5.addons = with pkgs; [
+          fcitx5-mozc
+          fcitx5-anthy
+          fcitx5-gtk
+      ];
+  };  
 
   environment.systemPackages = with pkgs; [
     vim
