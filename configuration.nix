a7c0f8547783a8f6607af983da8ac24d794fc7c3
Julius Michaelis <michaelis@fujitsu.com>
2023-12-12 10:48:47 +0900

    Set env elsewhere

--- configuration.nix
+++ configuration.nix
@@ -170,20 +170,20 @@
   };
   environment.sessionVariables = {
     WLR_NO_HARDWARE_CURSORS = "1";
-    QT_QPA_PLATFORM = "wayland";
-    CLUTTER_BACKEND = "wayland";
-    SDL_VIDEODRIVER = "wayland";
-    MOZ_ENABLE_WAYLAND = "1";
-    MOZ_WEBRENDER = "1";
-    XDG_SESSION_TYPE = "wayland";
-    XDG_CURRENT_DESKTOP = "sway";
-    QT_QPA_PLATFORMTHEME = "qt5ct";
-    GLFW_IM_MODULE = "fcitx";
-    GTK_IM_MODULE = "fcitx";
-    INPUT_METHOD = "fcitx";
-    XMODIFIERS = "@im=fcitx";
-    IMSETTINGS_MODULE = "fcitx";
-    QT_IM_MODULE = "fcitx";
+    #QT_QPA_PLATFORM = "wayland";
+    #CLUTTER_BACKEND = "wayland";
+    #SDL_VIDEODRIVER = "wayland";
+    #MOZ_ENABLE_WAYLAND = "1";
+    #MOZ_WEBRENDER = "1";
+    #XDG_SESSION_TYPE = "wayland";
+    #XDG_CURRENT_DESKTOP = "sway";
+    #QT_QPA_PLATFORMTHEME = "qt5ct";
+    #GLFW_IM_MODULE = "fcitx";
+    #GTK_IM_MODULE = "fcitx";
+    #INPUT_METHOD = "fcitx";
+    #XMODIFIERS = "@im=fcitx";
+    #IMSETTINGS_MODULE = "fcitx";
+    #QT_IM_MODULE = "fcitx";
   };
   i18n.inputMethod = {
     enabled = "fcitx5";
