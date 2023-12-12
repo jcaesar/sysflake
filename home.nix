b238d366c2e9a5f7dd1deea9b6189bc43de1cf9f
Julius Michaelis <michaelis@fujitsu.com>
2023-12-12 09:07:48 +0900

    home: nushell++

--- home.nix
+++ home.nix
@@ -1,7 +1,9 @@
-{
+(import ./shell.nix)
+// {
   home.username = "julius";
   home.homeDirectory = "/home/julius";
   home.stateVersion = "23.11";
 
   programs.home-manager.enable = true;
+  programs.command-not-found.enable = true;
 }
