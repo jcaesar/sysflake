ea9c143c4f47e2a303630c275880fc0a53e25d30
Julius Michaelis <michaelis@fujitsu.com>
2023-12-12 11:24:34 +0900

    Reformat networking

--- networking.nix
+++ networking.nix
@@ -1,66 +1,69 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
-  networking.useDHCP = false;
-  #boot.extraModulePackages = [config.boot.kernelPackages.wireguard];
-  systemd.network = {
-    enable = true;
-    networks."10-palmarola-eth-net" = {
-      matchConfig.Name = "enp0s31f6";
-      DHCP = "no";
-      address = ["10.13.25.2/24"];
-    };
-    netdevs.
-      "11-palmarola-wg-dev" = {
-      netdevConfig = {
-        Kind = "wireguard";
-        Name = "gozo";
-        MTUBytes = "1350";
+let
+  palmarolaPort = "enp0s31f6";
+  cameoPort = "enp2s0";
+in
+  {
+    config,
+    pkgs,
+    ...
+  }: {
+    networking.useDHCP = false;
+    #boot.extraModulePackages = [config.boot.kernelPackages.wireguard];
+    systemd.network = {
+      enable = true;
+      networks."10-palmarola-eth-net" = {
+        matchConfig.Name = palmarolaPort;
+        DHCP = "no";
+        address = ["10.13.25.2/24"];
       };
-      wireguardConfig = {
-        PrivateKeyFile = "/etc/secrets/gozo.pk";
-        ListenPort = 36749;
+      netdevs.      "11-palmarola-wg-dev" = {
+        netdevConfig = {
+          Kind = "wireguard";
+          Name = "gozo";
+          MTUBytes = "1350";
+        };
+        wireguardConfig = {
+          PrivateKeyFile = "/etc/secrets/gozo.pk";
+          ListenPort = 36749;
+        };
+        wireguardPeers = [
+          {
+            wireguardPeerConfig = {
+              PublicKey = "BThC89DqFj+nGtkCytNSskolwCijeyq/XDiAM8hQJRw=";
+              Endpoint = "10.13.25.1:53";
+              AllowedIPs = ["0.0.0.0/0"];
+              PersistentKeepalive = 29;
+            };
+          }
+        ];
       };
-      wireguardPeers = [
-        {
-          wireguardPeerConfig = {
-            PublicKey = "BThC89DqFj+nGtkCytNSskolwCijeyq/XDiAM8hQJRw=";
-            Endpoint = "10.13.25.1:53";
-            AllowedIPs = ["0.0.0.0/0"];
-            PersistentKeepalive = 29;
-          };
-        }
-      ];
+      networks."11-palmarola-wg-net" = {
+        matchConfig.Name = "gozo";
+        address = ["10.13.26.2/24"];
+        DHCP = "no";
+        dns = ["10.9.70.1 10.10.10.1"];
+        ntp = [];
+        gateway = [
+          "10.13.26.1"
+        ];
+        networkConfig = {
+          IPv6AcceptRA = false;
+        };
+      };
     };
-    networks."11-palmarola-wg-net" = {
-      matchConfig.Name = "gozo";
-      address = ["10.13.26.2/24"];
-      DHCP = "no";
-      dns = ["10.9.70.1 10.10.10.1"];
-      ntp = [];
-      gateway = [
-        "10.13.26.1"
-      ];
+
+    systemd.network.networks."10-cameo" = {
+      matchConfig.Name = cameoPort;
+      DHCP = "ipv4";
       networkConfig = {
         IPv6AcceptRA = false;
       };
+      dhcpV4Config = {
+        UseRoutes = false;
+        UseDNS = false;
+      };
+      linkConfig.RequiredForOnline = false;
     };
-  };
 
-  systemd.network.networks."10-cameo" = {
-    matchConfig.Name = "enp2s0";
-    DHCP = "ipv4";
-    networkConfig = {
-      IPv6AcceptRA = false;
-    };
-    dhcpV4Config = {
-      UseRoutes = false;
-      UseDNS = false;
-    };
-    linkConfig.RequiredForOnline = false;
-  };
-
-  environment.systemPackages = [pkgs.wireguard-tools];
-}
+    environment.systemPackages = [pkgs.wireguard-tools];
+  }
