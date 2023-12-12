fff8083f4c854f3908b19385bca543d407e8696d
Julius Michaelis <michaelis@fujitsu.com>
2023-12-12 09:47:45 +0900

    home manager, take 2 (still not working)

--- home.nix
+++ home.nix
@@ -1,9 +1,28 @@
-(import ./shell.nix)
-// {
-  home.username = "julius";
-  home.homeDirectory = "/home/julius";
-  home.stateVersion = "23.11";
+{
+  system,
+  nixpkgs,
+  home-manager,
+  ...
+}: let
+  username = "julius";
+  homeDirectory = "/home/${username}";
+  configHome = "${homeDirectory}/.config";
 
-  programs.home-manager.enable = true;
-  programs.command-not-found.enable = true;
-}
+  pkgs = import nixpkgs {
+    inherit system;
+    config.xdg.configHome = configHome;
+  };
+
+  shell = import ./shell.nix;
+in
+  shell
+  // {
+    programs.home-manager.enable = true;
+    programs.command-not-found.enable = true;
+    xdg = {inherit configHome;};
+
+    home = {
+      inherit username homeDirectory;
+      stateVersion = "23.11";
+    };
+  }
