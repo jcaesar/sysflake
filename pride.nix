4c0ac363f4508cb5f183c4dcaf1147602183150d
Julius Michaelis <gitter@liftm.de>
2024-01-28 20:24:22 +0900

    no schneegans :(

--- pride.nix
+++ pride.nix
@@ -91,11 +91,11 @@
     };
     desktopManager.gnome = {
       enable = true;
-      extraPackages = with pkgs;
-      with gnomeExtensions; [
-        desktop-cube
-        burn-my-windows
-      ];
+      #extraPackages = with pkgs;
+      #with gnomeExtensions; [
+      #  desktop-cube
+      #  burn-my-windows
+      #];
     };
   };
 
