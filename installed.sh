8fd6ee02bd18b4ff0a19635e4f01f1a5c6e78b72
Julius Michaelis <michaelis@fujitsu.com>
2024-03-11 22:05:01 +0900

    filter a few metapackages

--- installed.sh
+++ installed.sh
@@ -4,4 +4,5 @@ nix-store --gc --print-live \
 | sed -n 's/.drv$/&^*/p' \
 | xargs -r nix derivation show \
 | jq --arg host "$(hostname)" -rc 'to_entries[].value.env | select(.pname) | [.pname, .version, "nixpkgs", $host] | @csv' \
+| grep -vE \"bootstrap-stage[012]?-\|\"conda-shell
 | sort | uniq
