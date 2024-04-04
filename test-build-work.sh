4c8de202230d82b93558394eae858d1f8f16e64c
Julius Michaelis <michaelis@fujitsu.com>
2024-04-04 11:26:21 +0900

    unbreak shamo4

---
+++ test-build-work.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if ! which nixos-rebuild &>/dev/null; then
+	nix shell nixpkgs\#nixos-rebuild -c "$0" "$@"
+fi
+
+cd "$(dirname "$0")"
+
+export NIX_SSHOPTS="-q -oCompression=yes -oControlMaster=auto -oControlPath=/tmp/ssh-check-nix-build-%C -oControlPersist=60"
+
+exit=true
+for h in korsika capri shamo{0,2,4,6,7}; do
+	(
+		echo -en "\n$h\n$(echo $h | sed 's/./=/g')\n"
+		set -x
+		drv="$(nix eval -L --raw .#nixosConfigurations.$h.config.system.build.toplevel.drvPath --verbose)"
+		nix copy -vL -s --derivation --to ssh://shamo2 "$drv^*"
+		ssh $NIX_SSHOPTS shamo2 nix build -vL -o /tmp/check-nix-hostbuild-$h "$drv^*"
+	) && status=$? || status=$?
+	if test $status -ne 0; then
+		echo 1>&2 "$h failed"
+		exit=false
+	fi
+done
+
+$exit
