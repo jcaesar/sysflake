1dca8f6ef46ee87995bcdb521a14054264ad5447
Julius Michaelis <michaelis@fujitsu.com>
2024-04-11 09:13:27 +0900

    flake lock

--- test-build-work.sh
+++ test-build-work.sh
@@ -9,15 +9,15 @@ fi
 cd "$(dirname "$0")"
 
 export NIX_SSHOPTS="-q -oCompression=yes -oControlMaster=auto -oControlPath=/tmp/ssh-check-nix-build-%C -oControlPersist=60"
-
+target=shamo2
 exit=true
 for h in korsika capri shamo{0,2,4,6,7}; do
 	(
 		echo -en "\n$h\n$(echo $h | sed 's/./=/g')\n"
 		set -x
 		drv="$(nix eval -L --raw .#nixosConfigurations.$h.config.system.build.toplevel.drvPath --verbose)"
-		nix copy -vL -s --derivation --to ssh://shamo2 "$drv^*"
-		ssh $NIX_SSHOPTS shamo2 nix build -vL -o /tmp/check-nix-hostbuild-$h "$drv^*"
+		nix copy -vL -s --derivation --to ssh://$target "$drv^*"
+		ssh $NIX_SSHOPTS $target nix build -vL -o /tmp/check-nix-hostbuild-$h "$drv^*"
 	) && status=$? || status=$?
 	if test $status -ne 0; then
 		echo 1>&2 "$h failed"
