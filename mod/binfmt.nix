705b33a2197d61eb7ce9c4968ba6fb64092ac6a9
Julius Michaelis <michaelis@jp.fujitsu.com>
2024-06-09 12:30:43 +0900

    twiddle

--- mod/binfmt.nix
+++ mod/binfmt.nix
@@ -5,16 +5,14 @@
 }: let
   arches = ["aarch64-linux" "armv7l-linux" "riscv64-linux"];
   qus = pkgs.pkgsStatic.qemu-user.override {inherit arches;};
+  qusAttrs = sys: {
+    interpreter = "${qus.passthru.binaryFor sys}";
+    wrapInterpreterInShell = false;
+    preserveArgvZero = true;
+    matchCredentials = true;
+    fixBinary = true;
+  };
 in {
   boot.binfmt.emulatedSystems = arches ++ ["wasm32-wasi" "wasm64-wasi" "x86_64-windows"];
-  boot.binfmt.registrations = let
-    attrs = sys: {
-      interpreter = "${qus.passthru.binaryFor sys}";
-      wrapInterpreterInShell = false;
-      preserveArgvZero = true;
-      matchCredentials = true;
-      fixBinary = true;
-    };
-  in
-    lib.genAttrs arches attrs;
+  boot.binfmt.registrations = lib.genAttrs arches qusAttrs;
 }
