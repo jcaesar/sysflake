889ff527e2a0635655978f0c2d7e8c0887e970d9
Julius Michaelis <gitter@liftm.de>
2024-06-16 19:19:07 +0900

    ha

--- sys/pitivi.nix
+++ sys/pitivi.nix
@@ -123,5 +123,24 @@ in {
     };
   };
 
+  services.home-assistant = {
+    enable = true;
+    extraComponents = [
+      "met"
+      "radio_browser"
+      "media_player"
+      "zha"
+      "bluetooth_tracker"
+      "bluetooth_le_tracker"
+      "bluetooth"
+      "bluetooth_adapters"
+      "generic_thermostat"
+      "dhcp"
+    ];
+    config = {
+      default_config = {};
+    };
+  };
+
   system.stateVersion = "24.05";
 }
