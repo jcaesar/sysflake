4ffd67a203cc1dce62a95e10590bd847a7797f5f
Julius Michaelis <michaelis@fujitsu.com>
2024-07-01 21:33:49 +0900

    flake lock

--- flake.nix
+++ flake.nix
@@ -94,7 +94,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05-small";
-    home-manager.url = "github:nix-community/home-manager";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
