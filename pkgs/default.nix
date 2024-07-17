cfc27afbc0f353e960a99cbc4ee770a1f5ad6170
Julius Michaelis <michaelis@fujitsu.com>
2024-07-17 09:48:36 +0900

    package junix

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,6 +3,7 @@ pkgs: {
   polaris-fuse = pkgs.callPackage ./polaris-fuse.nix {};
   prometheus-nvml-exporter = pkgs.callPackage ./prometheus-nvml-exporter.nix {};
   pyanidb = pkgs.python3.pkgs.callPackage ./pyanidb.nix {};
+  junix = pkgs.python3.pkgs.callPackage ./junix.nix {};
   njx = pkgs.callPackage ./njx.nix {};
   rowserext = pkgs.callPackage ./rowserext.nix {};
   colmap = import ./colmap.nix pkgs;
