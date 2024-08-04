8a9b3c4a74ad7108a6d361d86bb00eabfdab730d
Julius Michaelis <gitter@liftm.de>
2024-08-04 22:27:16 +0900

    spitz v6

--- sys/spitz/configuration.nix
+++ sys/spitz/configuration.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: let
+{
+  pkgs,
+  lib,
+  ...
+}: let
   private = import ../../private.nix;
   wlan = "wlp1s0";
 in {
@@ -30,12 +34,58 @@ in {
       # keeps failing over, cache flushes, thrashing occurs
       dhcpV4Config.UseDNS = false;
     };
+    networks."11-ll" = {
+      matchConfig.Name = "enp3s0";
+      DHCP = "yes";
+      linkConfig.RequiredForOnline = false;
+    };
+    netdevs."12-he-tunnel" = {
+      netdevConfig = {
+        Name = "he-ipv6";
+        Kind = "sit";
+        MTUBytes = toString 1480;
+      };
+      tunnelConfig = {
+        Local = "192.168.0.243";
+        Remote = "74.82.46.6";
+        TTL = 255;
+      };
+    };
+    networks."13-he-tunnel" = {
+      matchConfig.Name = "he-ipv6";
+      address = ["2001:470:1f0a:c81::2/64" "2001:470:1f0b:c82::1/64"];
+      gateway = ["2001:470:1f0a:c81::1"];
+    };
   };
   networking.supplicant.${wlan} = {
     configFile.writable = true;
     configFile.path = "/etc/wpa_supplicant.conf";
   };
   networking.wireless.userControlled.enable = true;
+  # TODO: Modularize (with hostname option)
+  systemd.tmpfiles.rules = [
+    "D /run/he-tunnel-update 700 root root - -"
+  ];
+  systemd.services.he-tunnel-update = {
+    serviceConfig = {
+      ExecStart = pkgs.writeScript "he-tunnel-update" ''
+        set -eu
+        ${lib.getExe' pkgs.xh "xhs"} "https://$(cat /etc/secrets/he-tunnel-update-auth)@ipv4.tunnelbroker.net/nic/update" hostname==504322
+      '';
+      RootDirectory = "/run/he-tunnel-update";
+      BindReadOnlyPaths = ["/nix/store" "/etc/secrets/he-tunnel-update-auth"];
+    };
+  };
+  systemd.timers.he-tunnel-update = {
+    timerConfig = {
+      OnCalendar = "09:01:33 Asia/Tokyo"; # random
+      OnBootSec = "300s";
+    };
+    wantedBy = ["timers.target"];
+  };
+  njx.manual.he-tunnel = ''
+    Place $user:$pass file in /etc/secrets/he-tunnel-update-auth
+  '';
 
   boot.supportedFilesystems = ["bcachefs"];
   disko.devices.disk.diks = {
