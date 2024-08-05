5925a982ab020131e2c802e7fbc251806603ee03
Julius Michaelis <michaelis@fujitsu.com>
2024-08-06 08:44:16 +0900

    stop loss?

--- sys/drosophila.nix
+++ sys/drosophila.nix
@@ -37,11 +37,11 @@ in {
     };
   };
   systemd.timers.stop-loss = {
-    timerConfig = {
-      OnCalendar = "23:00:00 Asia/Tokyo";
-      Unit = "shutdown.target";
-    };
+    timerConfig.OnCalendar = "23:00:00 Asia/Tokyo";
     wantedBy = ["timers.target"];
+  };
+  systemd.services.stop-loss = {
+    serviceConfig.ExecStart = "/run/current-system/sw/bin/systemctl poweroff";
   };
   fileSystems."/home" = {
     device = "/dev/disk/by-label/homedisk";
