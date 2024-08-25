987eac5d08acecb4f789c2a71cf2304a0308f089
Julius Michaelis <gitter@liftm.de>
2024-08-25 16:19:38 +0900

    boot to the head

--- sys/lasta/configuration.nix
+++ sys/lasta/configuration.nix
@@ -16,6 +16,7 @@
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.initrd.systemd.enable = true;
+  boot.initrd.systemd.suppressedUnits = "tpm2.target"; # timeouts waiting on dev-tpmrm0
   hardware.cpu.intel.updateMicrocode = true;
 
   fileSystems."/" = {
