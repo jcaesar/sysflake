76fb003d1edb5638997cb5fca3ba21e0ac8c093b
Julius Michaelis <gitter@liftm.de>
2024-09-07 15:20:09 +0900

    pkgs/gegensprech derivation externalized, vump

--- pkgs/gegensprech.nix
+++ pkgs/gegensprech.nix
@@ -1,50 +1,12 @@
-let
-  name = "gegensprech";
-  author = "jcaesar";
+{
+  fetchFromGitHub,
+  callPackage,
+}: let
+  src = fetchFromGitHub {
+    owner = "jcaesar";
+    repo = "gegensprech";
+    rev = "2492e139a42c516df74e7c9011671e4441fe6295";
+    hash = "sha256-wxMGBayFbiiDgv+MUgrW1RKPrfyFXXf326TaJiHXNRU=";
+  };
 in
-  {
-    rustPlatform,
-    fetchFromGitHub,
-    lib,
-    cmake,
-    libopus,
-    libpulseaudio,
-    pkg-config,
-    stdenv,
-  }:
-    rustPlatform.buildRustPackage rec {
-      pname = name;
-      version = "0.1.0";
-
-      src = fetchFromGitHub {
-        owner = author;
-        repo = name;
-        rev = "eed4ca799fa8bc5236e61a23312d5129846bc28a";
-        hash = "sha256-el+JN2u4B/fCre+f5yFwKVhunwKT1wGpZ12mYImCF0w=";
-      };
-      cargoLock = {
-        lockFile = "${src}/Cargo.lock";
-        outputHashes = {
-          "ogg-opus-0.1.2" = "sha256-bHzM0xC8RiWD1wMFqJHpoaU43p3qR5lEwEsiYrzVY/A=";
-        };
-      };
-
-      nativeBuildInputs = [cmake pkg-config];
-      buildInputs = [libopus libpulseaudio];
-      buildFeatures = ["audio-as-lib"];
-      # libpulse-simple-sys links by name if pkgconfig fails,
-      # and that results in a a binary that can't be run
-      # postFixup = "${lib.getExe libtree} $out/bin/gegensprech";
-      postFixup = lib.optionalString (stdenv.buildPlatform.canExecute stdenv.hostPlatform) ''
-        $out/bin/gegensprech help
-      '';
-
-      meta = {
-        description = "Matrix push-to-talk";
-        license = lib.licenses.mit;
-        platforms = lib.platforms.linux;
-        maintainers = [lib.maintainers.${author}];
-        homepage = "https://github.com/${author}/${name}";
-        mainProgram = "${name}";
-      };
-    }
+  callPackage src {}
