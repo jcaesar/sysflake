ffd8a207f57564b5d0cda31baa297a25558a520f
Julius Michaelis <gitter@liftm.de>
2024-09-10 23:24:59 +0900

    nu env fix

--- mod/common-home-nushell-env.nix
+++ mod/common-home-nushell-env.nix
@@ -6,7 +6,9 @@
 }: {
   # workaround for there not being nushell support for environment.variables
   # can't be in home.nix because that doesn't have access to config(?), can't be in common.nix because of the key collision
-  home-manager.users.julius.programs.nushell.extraEnv = lib.mkIf config.njx.common ''
-    open ${pkgs.writeText "environment.variables.json" (builtins.toJSON config.environment.variables)} | load-env
-  '';
+  home-manager = lib.mkIf config.njx.common {
+    users.julius.programs.nushell.extraEnv = ''
+      open ${pkgs.writeText "environment.variables.json" (builtins.toJSON config.environment.variables)} | load-env
+    '';
+  };
 }
