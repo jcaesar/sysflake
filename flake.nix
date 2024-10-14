63eda53196a0707b9277eec08ed0a14f3a1674f4
Julius Michaelis <gitter@liftm.de>
2024-10-14 20:32:40 +0900

    small stuff not building again

--- flake.nix
+++ flake.nix
@@ -86,7 +86,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     disko.url = "github:nix-community/disko";
