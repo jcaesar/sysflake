a051971577899a6cde7b39f98679d2d787703f91
Julius Michaelis <michaelis@fujitsu.com>
2024-10-16 10:17:13 +0900

    plumbing for garbage

--- sys/shamo.nix
+++ sys/shamo.nix
@@ -183,5 +183,54 @@ in rec {
     wantedBy = ["timers.target"];
   };
 
+  services.vector = {
+    enable = true;
+    journaldAccess = true;
+    settings = {
+      sources.journal.type = "journald";
+      sinks.cw = {
+        type = "aws_cloudwatch_logs";
+        inputs = ["journal"];
+        group_name = "/fnet/shamo/syslog";
+        stream_name = "{{ host }}";
+        encoding.codec = "json";
+        region = "ap-northeast-1";
+        auth.credentials_file = "/run/credentials/vector.service/cloudwatch"; # I'd prefer to use "\${CREDENTIALS_DIRECTORY}/cloudwatch", but that fails the verify check
+        batch.timeout_secs = 10;
+        create_missing_group = true;
+        create_missing_stream = true;
+        proxy.enabled = true;
+        proxy.https = proxy;
+      };
+    };
+  };
+  systemd.services.vector = {
+    serviceConfig.LoadCredential = "cloudwatch:/etc/secrets/cloudwatch";
+    environment.VECTOR_LOG = "warn"; # It'll log "Putting events successfull" to the syslog, and then try to log that. Indefinitely.
+  };
+  njx.manual.vector-aws-cloudwatch = ''
+    1. Create access
+       ```
+       aws iam create-user --user-name shamo-log-appender
+       aws iam attach-user-policy --user-name shamo-log-appender --policy-arn arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs
+       aws iam create-access-key --user-name shamo-log-appender
+       ```
+    2. Create credential file at `/etc/secrets/cloudwatch`
+       ```
+       touch /etc/secrets/cloudwatch
+       chmod 400 /etc/secrets/cloudwatch
+       ```
+       ```
+       [default]
+       aws_access_key_id = (output from create-access-key)
+       aws_secret_access_key = (output from create-access-key)
+       ```
+    3. After vector's first run, fix log retention policy
+       ```
+       aws logs put-retention-policy --log-group-name /fnet/shamo/syslog --retention-in-days 30
+       ```
+       (Vector itself has functionality for that, but it requires allowing changing the retention policy, including shortening it unreasonably)
+  '';
+
   system.stateVersion = "23.05";
 }
