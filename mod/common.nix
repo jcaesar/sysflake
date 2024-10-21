609a6c36922f3afe1acc825054a3439f76f6ffb1
Julius Michaelis <michaelis@fujitsu.com>
2024-10-21 13:35:22 +0900

    stop hm generation littering

--- mod/common.nix
+++ mod/common.nix
@@ -5,6 +5,19 @@
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.users.julius = import ./home.nix;
+  home-manager.sharedModules = [
+    ({
+      lib,
+      pkgs,
+      config,
+      ...
+    }: {
+      home.activation.cleanGenerations = lib.hm.dag.entryAfter ["linkGeneration"] ''
+        run ${lib.getExe' pkgs.nix "nix-env"} $VERBOSE_ARG --delete-generations \
+          --profile ${config.xdg.stateHome}/nix/profiles/home-manager +1
+      '';
+    })
+  ];
 
   systemd.oomd.enableUserSlices = true;
 
