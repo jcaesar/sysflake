84bf2b40e8d788706bcda5fa73043d4da373f8da
Julius Michaelis <gitter@liftm.de>
2024-11-17 15:48:04 +0900

    blinky

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,7 @@ pkgs: prev: {
   cyrly = pkgs.callPackage ./cyrly.nix {};
   prometheus-nvml-exporter = pkgs.callPackage ./prometheus-nvml-exporter.nix {};
   gegensprech = pkgs.callPackage ./gegensprech.nix {};
+  seeed-2mic-blinky = pkgs.callPackage ./seeed-2mic-blinky.nix {};
   pyanidb = pkgs.python3.pkgs.callPackage ./pyanidb.nix {};
   junix = pkgs.python3.pkgs.callPackage ./junix.nix {};
   njx = pkgs.callPackage ./njx.nix {};
